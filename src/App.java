41b2a3ae2e054663609605484aa1c693692eed81
Wesley <wesleycharles01@hotmail.com>
2024-10-21 11:37:46 -0300

    inicio

---
+++ src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digita o nome completo: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o número da agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número: ");
+        int numero = scanner.nextInt();
+
+        System.out.println("Adicione um valor inicial a conta: ");
+        int saldo = scanner.nextInt();
+
+
+
+        scanner.close();
+
+
+
+
+        Conta conta = new Conta(numero, agencia, nomeCliente, saldo);
+
+        conta.contaCriada();
+    }
+}
---
+++ src/Conta.java
@@ -0,0 +1,18 @@
+public class Conta {
+    private int numero = 0000;
+    private String agencia = "067-8";
+    private String nomeCliente = " ";
+    private int saldo = 0;
+
+    public Conta(int numero, String agencia, String nomeCliente, int saldo) {
+        
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public void contaCriada() {
+        System.out.println("Olá " + nomeCliente + " , obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo: " + saldo + ", já está disponível para saque");
+    }
+}
